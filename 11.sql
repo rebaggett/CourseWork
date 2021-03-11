1deb7617e59d305c5a6695862e2436f577b35bd0
rebaggett <50774889+rebaggett@users.noreply.github.com>
2021-03-11 10:38:47 -0600

    Add files via upload

---
+++ 11.sql
@@ -0,0 +1,6 @@
+SELECT title FROM
+movies JOIN ratings ON movies.id = ratings.movie_id
+WHERE movies.id IN(
+SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id
+WHERE people.name = "Chadwick Boseman")
+ORDER BY ratings.rating DESC LIMIT 5
---
+++ 13.sql
@@ -0,0 +1,4 @@
+SELECT name FROM people WHERE name != "Kevin Bacon" AND id IN(
+SELECT person_id FROM stars WHERE movie_id IN(
+SELECT movies.id FROM movies JOIN stars ON movies.id = stars.movie_id WHERE stars.person_id IN(
+SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)))
